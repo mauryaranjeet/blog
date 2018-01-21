1bb55d344c760b5fd8cec77153ad438431873bcf
Ranjeet Maurya <ranjeet.maurya@sodelsolutions.com>
2018-01-21 22:39:59 +0530

    tinymice changes

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -2,7 +2,15 @@
 //= require tinymce
 
 $(document).ready(function() {
-  tinyMCE.init({
-      mode: 'textareas'
-        });
+	tinymce.init({
+	    selector: "textarea",
+	    plugins: [
+	        "advlist autolink lists link image charmap print preview anchor",
+	        "searchreplace visualblocks code fullscreen",
+	        "insertdatetime media table contextmenu paste"
+	    ],
+	    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
+  		images_upload_url: 'postAcceptor.php',
+  		automatic_uploads: false		
+	});
 });
