80f8318ef3de1da14ecda8d613f7031fa5c21569
Ranjeet Maurya <ranjeet.maurya@sodelsolutions.com>
2018-01-28 00:04:54 +0530

    per page 30

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
 
-  paginates_per 2
+  paginates_per 30
   has_attached_file :image_description, styles: { medium: "300x300>", thumb: "100x100>", banner: "900x300" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image_description, content_type: /\Aimage\/.*\z/
 
