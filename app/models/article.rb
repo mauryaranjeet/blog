736862a35220c7ef66e29fbdad6f3c5595e54e74
Ranjeet Maurya <ranjeet.maurya@sodelsolutions.com>
2018-02-10 10:16:34 +0530

    added finders

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,8 +5,9 @@ class Article < ApplicationRecord
   has_attached_file :image_description, styles: { medium: "300x300>", thumb: "100x100>", banner: "900x300" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image_description, content_type: /\Aimage\/.*\z/
 
-  friendly_id :heading, use: :slugged
+  friendly_id :heading, :use =>  [:slugged, :finders]
   has_many :sections
 
   accepts_nested_attributes_for :sections, :reject_if => lambda { |param| param[:content].blank? }, :allow_destroy => true
+
 end
